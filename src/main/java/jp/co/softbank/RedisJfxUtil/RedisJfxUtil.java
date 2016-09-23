6183a1a8e9205e98a67d0ae0edde80fd453ce6b4
å <taka0206@me.com>
2016-09-23 13:25:12 +0900

    [add] fxscript sample

---
+++ src/main/java/jp/co/softbank/RedisJfxUtil/RedisJfxUtil.java
@@ -0,0 +1,29 @@
+package jp.co.softbank.RedisJfxUtil;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class RedisJfxUtil extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
+		Scene scene = new Scene(root, 300, 150);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
