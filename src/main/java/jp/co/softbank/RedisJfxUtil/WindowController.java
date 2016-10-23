e91d8acca540f86bd8e75db4194e517de4326d14
takahiko yoshizawa <taka0206@me.com>
2016-10-23 14:25:33 +0900

    [機能追加] 機能追加

--- src/main/java/jp/co/softbank/RedisJfxUtil/WindowController.java
+++ src/main/java/jp/co/softbank/RedisJfxUtil/WindowController.java
@@ -1,21 +1,27 @@
 package jp.co.softbank.RedisJfxUtil;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.commons.lang.StringUtils;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import redis.clients.jedis.Jedis;
 
 public class WindowController {
 	
 	@FXML
-	private Label label1;
-	
-	@FXML
-	private TextField text1;
-	
-	
-	@FXML
 	private TextField redisUri;
+	
+	@FXML
+	private TextField redisPort;
 	
 	@FXML
 	private TextField auth;
@@ -29,9 +35,37 @@ public class WindowController {
 	@FXML
 	private TextField hashValue;
 	
+	protected void validateCheck(String redisUri, String port) {
+		if (StringUtils.isBlank(redisUri) || StringUtils.isBlank(port)) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	protected Jedis connectRedis(String redisUri, int port, String auth) 
+			throws UnsupportedEncodingException, IOException {
+		
+		Path path = FileSystems.getDefault().getPath(
+				System.getProperty("user.home") + 
+				File.separator + 
+				".redisutil_conf.properties");
+		Jedis jedis = new Jedis(redisUri, port, 1000);
+		jedis.connect();
+		StringBuilder sb = new StringBuilder();
+		sb.append(redisUri).append(",").append(port).append(",");
+		if (StringUtils.isNotBlank(auth)) {
+			auth = auth.trim();
+			sb.append("," + auth);
+			jedis.auth(auth);
+		}
+		Files.write(path, sb.toString().getBytes("UTF-8"));
+		return jedis;
+	}
+	
 	@FXML
 	protected void getKeyAllAction(ActionEvent event) {
-		System.out.println("keyAllGet呼ばれた");
+		validateCheck(redisUri.getText(), redisPort.getText());
+		JeconnectRedis()
+		
 	}
 	
 	@FXML
